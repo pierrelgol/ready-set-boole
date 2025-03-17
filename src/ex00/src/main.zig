c29f3ca32ee6427c912f19317f14a38652a701a9
PierreLgol <plgol.perso@gmail.com>
2025-03-17 12:33:00 +0100

    upt

--- src/ex00/src/main.zig
+++ src/ex00/src/main.zig
@@ -1,39 +1,59 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
-
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
-
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 const std = @import("std");
+
+pub fn add(a: u1024, b: u1024) u1024 {
+    var carry: u32 = a & b;
+    var result: u32 = a ^ b;
+    while (carry != 0) {
+        const new_carry = carry << 1;
+        carry = result & new_carry;
+        result ^= new_carry;
+    }
+    return result;
+}
+
+test add {
+    var random_generator = std.Random.DefaultPrng.init(std.testing.random_seed);
+    const random = random_generator.random();
+
+    const test_number = random.uintAtMost(u16, std.math.maxInt(u16));
+    for (0..test_number) |_| {
+        const num1 = random.uintAtMost(u32, std.math.maxInt(u32));
+        const num2 = random.uintAtMost(u32, std.math.maxInt(u32));
+        try std.testing.expect(add(num1, num2) == (num1 +% num2));
+    }
+}
+const add_visual =
+    \\ pub fn add({d}: u32, {d}: u32) u32 {{
+    \\     var {d}: u32 = {b} & {b};
+    \\     var {d}: u32 = {b} ^ {b};
+    \\     while ({d} != 0) {{
+    \\         const {b} = {b} << 1;
+    \\         {b} = {b} & {b};
+    \\         {b} ^= {b};
+    \\     }}
+    \\     return {d};
+    \\ }}
+;
+pub fn main() !void {
+    var buffer: [std.heap.pageSize()]u8 = undefined;
+    var fba: std.heap.FixedBufferAllocator = .init(buffer[0..]);
+    const allocator = fba.allocator();
+    _ = allocator;
+
+    const stdin = std.io.getStdIn().reader();
+    _ = stdin;
+    const stdout = std.io.getStdOut().writer();
+
+    const a: u32 = 25;
+    const b: u32 = 30;
+    var carry: u32 = a & b;
+    var result: u32 = a ^ b;
+    var new_carry: u32 = 0;
+    try stdout.print(add_visual, .{ a, b, carry, a, b, result, a, b, carry, new_carry, carry, carry, result, new_carry, result, new_carry, result });
+    while (carry != 0) {
+        new_carry = carry << 1;
+        carry = result & new_carry;
+        result ^= new_carry;
+        try stdout.print(add_visual, .{ a, b, carry, a, b, result, a, b, carry, new_carry, carry, carry, result, new_carry, result, new_carry, result });
+    }
+}
