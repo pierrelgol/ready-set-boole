5aaca2dc610cba862683f3907697e0417da3f8af
PierreLgol <plgol.perso@gmail.com>
2025-03-18 13:50:22 +0100

    upt

--- src/ex02/src/main.zig
+++ src/ex02/src/main.zig
@@ -1,39 +1,58 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+fn GrayTable(comptime n: usize) type {
+    if (n == 0) @compileError("0 sized types are not supported");
+    if (n >= 16) @compileError("n sized types above or equal 16 are not supported");
+    return struct {
+        const Self = @This();
+        const State = std.math.pow(u16, 2, n) - 1;
+        pub const IntegerType = std.math.IntFittingRange(0, State);
+        pub const Length = std.math.maxInt(IntegerType);
+        table: [Length]IntegerType = blk: {
+            var buf: [Length]IntegerType = undefined;
+            @setEvalBranchQuota(20000000);
+            for (0..Length) |i| {
+                buf[i] = (i ^ (i >> 1));
+            }
+            break :blk buf;
+        },
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+        pub fn get(self: Self, num: anytype) IntegerType {
+            return self.table[std.math.lossyCast(IntegerType, num)];
+        }
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
-
-    try bw.flush(); // Don't forget to flush!
-}
-
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
+        pub fn print(self: Self) void {
+            for (0..Length) |i| {
+                std.debug.print("{d:0>4} -> {b:0>10} : {b:0>10}\n", .{ i, i, self.table[i] });
+            }
         }
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+}
+fn binaryToGray(comptime n: u32) u32 {
+    return n ^ (n >> 1);
 }
 
-const std = @import("std");
+fn grayToBinary(comptime n: u32) u32 {
+    var nb = n;
+    nb ^= nb >> 16;
+    nb ^= nb >> 8;
+    nb ^= nb >> 4;
+    nb ^= nb >> 2;
+    nb ^= nb >> 1;
+    return nb;
+}
+
+const stdin = std.io.getStdIn().reader();
+const stdout = std.io.getStdOut().writer();
+
+pub fn main() !void {
+    var buffer: [std.heap.pageSize()]u8 = undefined;
+    var fba: std.heap.FixedBufferAllocator = .init(buffer[0..]);
+    const allocator = fba.allocator();
+    const table = GrayTable(15){};
+    while (true) {
+        const number = stdin.readUntilDelimiterAlloc(allocator, '\n', 6) catch continue;
+        const num = std.fmt.parseUnsigned(u16, number, 10) catch 0;
+        std.debug.print("integer : {d:>6} | gray : {d:>6} \n", .{ num, table.get(num) });
+    }
+}
