491134ba8d8b928592f02faa3a798f7a54b67292
PierreLgol <plgol.perso@gmail.com>
2025-04-06 15:47:50 +0200

    updated root

--- src/root.zig
+++ src/root.zig
@@ -1,9 +1,10 @@
 const std = @import("std");
 pub const Ast = @import("common/Ast.zig");
+pub const AstNode = @import("common/AstNode.zig").AstNode;
 pub const Token = @import("common/Token.zig").Token;
+pub const TokenKind = @import("common/Token.zig").Kind;
+pub const Lexer = @import("common/Lexer.zig").Lexer;
+pub const Parser = @import("common/Parser.zig").Parser;
+
 pub const Repl = @import("common/Repl.zig");
-pub const Lexer = @import("common/Lexer.zig");
-pub const LinkedList = @import("common/LinkedList.zig").LinkedList;
-pub const StackUnmanaged = @import("common/Stack.zig").StackUnmanaged;
-pub const Stack = @import("common/Stack.zig").Stack;
 pub const Interpreter = @import("common/Interpreter.zig");
